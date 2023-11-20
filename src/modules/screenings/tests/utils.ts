2f60f954d35ff2be164eed4637012676c457d30e
Dorota Jez <depadmil@gmail.com>
2023-11-20 11:20:17 +0100

    create screenings fixture

--- src/modules/screenings/tests/utils.ts
+++ src/modules/screenings/tests/utils.ts
@@ -10,3 +10,27 @@ export const screeningRecordFactory = (
   movieId: 2343,
   ...overrides,
 })
+
+export const createScreeningsForTest = () => [
+  { id: 5832, timestamp: '2023-11-19 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 3202, timestamp: '2023-11-20 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 293, timestamp: '2023-11-21 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 200, timestamp: '2023-11-22 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  {
+    id: 38902,
+    timestamp: '2023-11-23 17:29:50',
+    ticketsLeft: 5,
+    movieId: 2343,
+  },
+  { id: 3892, timestamp: '2023-11-24 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 3823, timestamp: '2023-11-25 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 43, timestamp: '2023-11-26 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 389, timestamp: '2023-11-27 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 390, timestamp: '2023-11-28 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 4023, timestamp: '2023-11-29 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 4903, timestamp: '2023-11-29 17:29:56', ticketsLeft: 5, movieId: 2343 },
+  { id: 4930, timestamp: '2023-11-30 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 490, timestamp: '2023-12-01 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 48, timestamp: '2023-12-19 17:29:50', ticketsLeft: 5, movieId: 2343 },
+  { id: 900, timestamp: '2023-12-7 17:29:50', ticketsLeft: 5, movieId: 2343 },
+]
